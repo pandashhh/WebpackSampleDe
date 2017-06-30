b744518212347ee0d4fbe29127bc9cef5e40f783
pandashhh <Jjsunghack1989@gmail.com>
2017-06-30 11:15:46 +0200

    34 Refactoring for Vendor Splitting

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+var path = require('path');
+//array of the name of file we wann include seperate in this file
+//moduals dependencies
+const VENDOR_LIBS = [
+  'react','lodash','redux','faker','react-redux','react-dom','react-router','react-input-range',
+  'redux-form','redux-thunk'
+];
+
+module.exports = {
+  entry: {
+    bundle:'./src/index.js',
+    vendor: VENDOR_LIBS
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module:{
+    rules:[
+      {
+        use:'babel-loader',
+        test:/\.js$/,
+        exclude:/node_modules/
+      },
+      {
+        use:['style-loader', 'css-loader'],
+        test:/\.css$/
+      }
+    ]
+  }
+};
